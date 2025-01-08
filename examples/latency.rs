f88c04b9e91e2887a2e2c772dc862af7c906b96e
Brad Luo <brad.luo@mirana.tech>
2025-01-08 15:25:47 +0800

    fix infinite

--- examples/latency.rs
+++ examples/latency.rs
@@ -1,7 +1,7 @@
 mod latency_stat;
 
 use std::sync::Arc;
-use std::sync::atomic::Ordering;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::time::Duration;
 use diatomic_waker::WakeSink;
 use minstant::{Atomic, Instant};
@@ -18,26 +18,39 @@ fn main() {
 
   let wake_src = wake_sink.source();
   let send_time = time.clone();
+  let done = Arc::new(AtomicBool::new(false));
+  let done_clone = done.clone();
   let sender = std::thread::spawn(move || {
     for _ in 0..ROUND {
       std::thread::sleep(Duration::from_millis(5));
       send_time.store(Instant::now(), Ordering::Relaxed);
       wake_src.notify();
     }
+    println!("[send] done");
+    done_clone.store(true, Ordering::Relaxed);
+    wake_src.notify();
   });
 
-  std::thread::spawn(move || {
+  let receiver = std::thread::spawn(move || {
     rt().block_on(async move {
       let mut latency_stat = LatencyStat::with_max(10_000);
-      for _ in 0..ROUND {
+      for i in 0..ROUND {
         let latency_us = wake_sink.wait_until(|| {
           let t = time.swap(Instant::ZERO, Ordering::Relaxed);
           if t > Instant::ZERO {
             Some(t.elapsed().as_micros())
           } else {
-            None
+            if done.load(Ordering::Relaxed) {
+              Some(u128::MAX)
+            } else {
+              None
+            }
           }
         }).await;
+        if latency_us == u128::MAX {
+          println!("[receiver] got {}/{} exit", i, ROUND);
+          break;
+        }
         latency_stat.record_latency(latency_us as u64);
         // total_latency_us += latency_us;
         // num += 1;
@@ -48,9 +61,10 @@ fn main() {
       latency_stat.evaluate(&mut perf_data);
       println!("latency: {}", perf_data);
     })
-  }).join().unwrap();
+  });
 
   sender.join().unwrap();
+  receiver.join().unwrap();
 }
 
 #[cfg(target_os = "linux")]
